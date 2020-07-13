6185b83d5db21e146159539aa814bc6bda200ca2
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-14 03:26:59 +0400

    Write Entity Layer

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductID { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public short UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+}
