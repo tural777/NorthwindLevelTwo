9ce57872ed795c32b7c40c2014cdef4e0610e724
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-14 12:58:36 +0400

    Write Business Layer

---
+++ Business/Concrete/CategroyManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategroyManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+    
+        public CategroyManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetList();
+        }
+    }
+}
