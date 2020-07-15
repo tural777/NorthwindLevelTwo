0305c6ca590a53eb4ec1f6deb8738d19a2bc9055
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-15 22:27:01 +0400

    AddDomainModelsAndSessionExtensionMethods

---
+++ Business/Concrete/CartManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Entities.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CartManager : ICartService
+    {
+
+        public void AddToCart(Cart cart, Product product)
+        {
+            CartLine cartLine = cart.CartLines.FirstOrDefault(c => c.Product.ProductID == product.ProductID);
+            if (cartLine != null)
+            {
+                cartLine.Quantity++;
+                return;
+            }
+
+            cart.CartLines.Add(new CartLine { Product = product, Quantity = 1 });
+        }
+
+        public List<CartLine> List(Cart cart)
+        {
+            return cart.CartLines;
+        }
+
+        public void RemoveFromCart(Cart cart, int productId)
+        {
+            cart.CartLines.Remove(cart.CartLines.FirstOrDefault(c => c.Product.ProductID == productId));
+        }
+    }
+}
---
+++ Entities/DomainModels/Cart.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DomainModels
+{
+    public class Cart : IDomainModel
+    {
+        public List<CartLine> CartLines { get; set; }
+
+        public Cart()
+        {
+            CartLines = new List<CartLine>();
+        }
+    }
+}
